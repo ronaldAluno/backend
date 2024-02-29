680e948459fcfc540387738f4172aa937a8751af
RonaldIESB <ronald.silva@iesb.edu.br>
2024-02-29 10:57:29 -0300

    Atualização 2.0

--- Aulas/Aula02/index.js
+++ Aulas/Aula02/index.js
@@ -1,45 +1,79 @@
-const readline = require('readline-sync');
+const readline = require("readline-sync");
 
 const produtos = [];
 
 function criar() {
-    const nome = readline.question("Informe o nome do produto: ")
-    const preco = readline.question("Informe o preco do produto: ")
+  const nome = readline.question("Informe o nome do produto: ");
+  const preco = readline.question("Informe o preco do produto: ");
+  const novo = { nome, preco }; // new Object(nome, preco)
+  produtos.push(novo);
 }
 
 function listar() {
-    produtos.forEach((produto) =>
-    console.log(produto, nome, "-", produto.preco));
+  produtos.forEach((produto) => console.log(produto.nome, "-", produto.preco));
 }
 
+function buscar() {
+  const nome = readline.question("Informe o nome do produto: ");
+  const buscou = produtos.find((produto) => produto.nome === nome);
+  if (buscou) {
+    console.log(buscou.nome, "-", buscou.preco);
+  } else {
+    console.log("Produto não encontrado");
+  }
+}
+
+function atualizar() {
+  const nome = readline.question("Informe o nome do produto: ");
+  const buscou = produtos.find((produto) => produto.nome === nome);
+  if (buscou) {
+    const outroNome = 
+      readline.question("Informe outro nome para o produto: ");
+    const outroPreco = 
+      readline.question("Informe outro preco para o produto: ");
+    buscou.nome = outroNome;
+    buscou.preco = outroPreco;
+  } else {
+    console.log("Produto não encontrado");
+  }
+}
 
 function menu() {
-    console.log("1. Listar");
-    console.log("2. Criar");
-    console.log("3. Buscar");
-    console.log("4. Atualizar");
-    console.log("5. Remover");
-    console.log("6. Sai");
+  console.log("1. Listar");
+  console.log("2. Criar");
+  console.log("3. Buscar");
+  console.log("4. Atualizar");
+  console.log("5. Remover");
+  console.log("6. Sair");
 }
 
 function escolherOpcao(opcao) {
-    switch (opcao) {
-        case "1": break;
-        case "2": break;
-        case "3": break;
-        case "4": break;
-        case "5": break;
-        case "6": process.exit(0);
-        default: console.log('Opcao invalida');
-    }
+  switch (opcao) {
+    case "1":
+      listar();
+      break;
+    case "2":
+      criar();
+      break;
+    case "3":
+      buscar();
+      break;
+    case "4":
+      break;
+    case "5":
+      break;
+    case "6":
+      process.exit(0);
+    default:
+      console.log("Opcao invalida");
+  }
+  readline.question("Pressione ENTER para continuar...");
 }
 
 function main() {
-    while (true) {
-        menu();
-        const opcao = readline.question("Entre com uma opcao: ")
-        escolherOpcao(opcao);
-    }
+  while (true) {
+    menu();
+    const opcao = readline.question("Entre com uma opcao: ");
+    escolherOpcao(opcao);
+  }
 }
-
-main();
